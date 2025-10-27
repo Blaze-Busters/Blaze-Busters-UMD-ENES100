c8c61c4f8cfa22cd44e6c71205ad56dbef62410d
Michael Ladyzhensky <mlady@terpmail.umd.edu>
2025-10-27 17:30:02 -0400

    Update zone_template.py

--- zone_template.py
+++ zone_template.py
@@ -1,62 +1,95 @@
+'''
+READ ME
+The way refresh_pose works is that it is a function that updates the position of the OTV every time it is called. 
+You have to manually call it in every while loop where position is important. 
+The benefit of the function is that it calls all three positions at once. 
+I have not put this function in most while loops bcs it would get ugly to read. 
+Also please check if the code correctly switches between zones :)
+'''
+
+
+import time
 from enes100 import enes100
 from machine import Pin, PWM
-button_start = Pin(16, Pin.IN, Pin.PULL_UP)
-ultrasound_front = #value from front ultrasound sensor
-ultrasound_left = #value from left ultrasound sensor
-ultrasound_right = #value from right ultrasound sensor
 
-while True: #figure out where to put it
-    time.sleep(1)
-    x = enes100.x()
-    y = enes100.y()
-    theta = enes100.theta()
+UPDATE_MS = 1000 #update position every 1 second
+#zone maxes
+Z1_MAX = 0.80
+Z2_MAX = 2.80
+Z3_MAX = 3.80
 
+button_start = Pin(16, Pin.IN, Pin.PULL_UP) #rewire and stuff
 
-    if(button_start):
-        while(x < 0.8): #while in zone 1
-            if(enes100.y<1.3): #if at bottom position
-                while(!((theta>1.541)&&(theta<1.599))): #if not in wanted range
-                    #spin in place to face candle (facing certain theta)
-                #move forward until given cordinate
-            else: #if at top start
-                while(!((theta<-1.541)&&(theta>-1.599))):
-                    #spin in place to face candle (facing certain theta))
-                #move forward until given cordinate
+# pose cache
+x = y = theta = None
+_last_update = 0
 
-            #do stuff to put out candles and all that fun stuff
-            #back up 10 inches, turn 90 degress to face finish line
-            #move forward 15 inches, turn left. Move forward until near wall, turn right
-            #move forward
-            
-        while(<.79x<2.8): #while in zone 2
-            while(ultrasound_front>10):
-                #move forward
+def refresh_pose():
+    global x, y, theta, _last_update
+    now = time.ticks_ms()
+    if time.ticks_diff(now, _last_update) >= UPDATE_MS:
+        x = enes100.x()
+        y = enes100.y()
+        theta = enes100.theta()
+        _last_update = now
 
-            #obstacle avoidance Round 1
-            if(ultrasonic_front<10):
-                if(y>1.5):
-                    #turn right 90 degrees
-                    while(ultrasound_left<=10):
-                        #move forward
-                    #turn left 90 degrees
-                elif(y>0.8):
-                    #turn right 90 degrees
-                    while(ultrasound_left<=10):
-                        #move forward
-                    #turn left 90 degrees
-                else:
-                     #turn left 90 degrees
-                     while(ultrasound_left<=10):
-                         #move forward
-                    #turn right 90 degrees
+# defined states and when it switches
+IDLE, ZONE1, ZONE2, ZONE3, DONE = range(5)
+state = IDLE
 
-            #copy and paste this code 2 more times when finished :p
-                    
-        while(2.79<x<3.8): #zone3
-            if(y<1.5): #if so its easier to write an else statement telling OTV to move forward if it is alr going to go under the bar
-                #turn OTV left 90 degrees
-                while(y<1.5):
-                    #move forward
-            else:
-                #move forward
-#DONE
+while True:
+    refresh_pose()
+    time.sleep_ms(10)
+
+    # start once
+    if state == IDLE and button_start.value() == 0:
+        state = ZONE1
+
+    if state == ZONE1: #Zone 1
+         while (x < 0.8):
+           if (y < 1.3):
+               while not (1.541 < theta < 1.599):  # face +pi/2
+                   #spin to face correct direction
+               # move forward until given coordinate
+           else:
+               while not (-1.599 < theta < -1.541):  # face -pi/2
+                   #spin to face correct diretion
+                   
+               # move forward until given coordinate
+           # extinguish candles etc.
+        #------------------------------------------
+        # transition condition:
+        if x is not None and x >= Z1_MAX:
+            state = ZONE2
+
+    elif state == ZONE2: #Zone 2
+         while (0.79 < x < 2.8):
+           while (ultrasound_front > 10):
+               # move forward
+           if (ultrasound_front < 10):
+               if (y > 1.5):
+                   # right 90, forward until left clear, left 90
+               elif (y > 0.8):
+                   # same
+               else:
+                   # left 90, forward until left clear, right 90
+           # repeat pattern above 2 more times
+        # ------------------------------------------
+        if x is not None and x >= Z2_MAX:
+            state = ZONE3
+
+    elif state == ZONE3: #Zone 3
+         while (2.79 < x < 3.8):
+           if (y < 1.5):
+               # turn left 90
+               while (y < 1.5):
+                   # move forward
+           else:
+               # move forward
+        # ------------------------------------------
+        if x is not None and x >= Z3_MAX:
+            state = DONE
+
+    elif state == DONE:
+        # finished/stop motors?
+        pass

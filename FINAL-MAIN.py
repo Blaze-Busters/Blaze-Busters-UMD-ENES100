de9be909ad014568cb28e361a99e669db5afbe9d
Michael Ladyzhensky <mlady@terpmail.umd.edu>
2025-11-06 16:29:44 -0500

    Update FINAL-MAIN.py

--- FINAL-MAIN.py
+++ FINAL-MAIN.py
@@ -52,19 +52,40 @@ spin(2,50) #spin forward
 spin(5,-100) #spin backward
 '''
 
-#ULTRASOUND 
-#Varun pls finish this section love ya
-def distance_cm(): 
-    TRIG.value(0)
-    sleep_us(2)
-    TRIG.value(1)
-    sleep_us(10)
-    TRIG.value(0)
+#ULTRASOUND
+#fix threading
+def distance_cm(trig, echo):
+    trig.value(0)
+    time.sleep_us(2)
+    trig.value(1)
+    time.sleep_us(10)
+    trig.value(0)
     
-    duration = time_pulse_us(ECHO, 1, 30000)  # 30ms timeout
-    
+    duration = machine.time_pulse_us(echo, 1, 30000)  # wait for echo HIGH max 30ms
     dist_cm = (duration / 2) * 0.0343
     return dist_cm
+
+#sensor readings
+front_sensor = 0
+left_sensor_side = 0
+right_sensor_side = 0
+left_sensor_down = 0
+right_sensor_down = 0
+
+def update_sensors():
+    global front_sensor, left_sensor_side, right_sensor_side, left_sensor_down, right_sensor_down
+    while True:
+        front_sensor = distance_cm(TRIG5, ECHO5)
+        left_sensor_side = distance_cm(TRIG1, ECHO1)
+        right_sensor_side = distance_cm(TRIG2, ECHO2)
+        left_sensor_down = distance_cm(TRIG3, ECHO3)
+        right_sensor_down = distance_cm(TRIG4, ECHO4)
+
+        time.sleep(0.1)   # Update at 10Hz
+
+
+# Start the thread on ESP32
+_thread.start_new_thread(update_sensors, ())
 '''GUIDE: distance=distance_cm(trig,echo)'''
 
 #MOTOR FUNCTIONS

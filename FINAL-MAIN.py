7013bbc93dfdb225e8344b62e675c321097cadbc
Michael Ladyzhensky <mlady@terpmail.umd.edu>
2025-12-02 10:43:23 -0500

    Update FINAL-MAIN.py

--- FINAL-MAIN.py
+++ FINAL-MAIN.py
@@ -243,6 +243,29 @@ enes100.print(f"Candles Lit: {fire_emoji}")
 '''
 
 # ----------------- ORIENTATION FUNCTION -----------------
+import _thread
+import time
+from machine import Pin, time_pulse_us
+
+# ===== Sensors =====
+left_sensor_down = 0
+right_sensor_down = 0
+
+TRIG5 = Pin(5, Pin.OUT)
+ECHO5 = Pin(36, Pin.IN)
+TRIG4 = Pin(27, Pin.OUT)
+ECHO4 = Pin(35, Pin.IN)
+
+def distance_cm(trig, echo):
+    trig.value(0)
+    time.sleep_us(2)
+    trig.value(1)
+    time.sleep_us(10)
+    trig.value(0)
+    duration = time_pulse_us(echo, 1, 30000)
+    dist_cm = (duration / 2) * 0.0343
+    return dist_cm
+
 def classify_position(left_distance, right_distance):
     option_A = (9.4 <= left_distance <= 9.7) and (6.7 <= right_distance <= 7.3)
     option_B = (11.8 <= left_distance <= 12.3) and (9.4 <= right_distance <= 9.6)
@@ -259,6 +282,27 @@ def classify_position(left_distance, right_distance):
         return "Option D"
     else:
         return "Unknown — values do not match any option."
+
+def update_sensors():
+    global left_sensor_down, right_sensor_down
+    while True:
+        left_sensor_down = distance_cm(TRIG4, ECHO4)
+        right_sensor_down = distance_cm(TRIG5, ECHO5)
+        time.sleep(0.1)
+
+# start thread
+_thread.start_new_thread(update_sensors, ())
+
+# give thread time to update values
+time.sleep(0.2)
+
+
+while True:
+    print("left distance:", left_sensor_down)
+    print("right distance:", right_sensor_down)
+    time.sleep(0.5)
+
+#print(classify_position(left_sensor_down, right_sensor_down))
 
 # ----------------- VICTORY DANCE -----------------
 def victory_dance():

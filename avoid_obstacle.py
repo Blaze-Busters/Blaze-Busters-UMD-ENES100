a0401b0fbdec502e3f568da99fc99f5724fd47cc
Michael Ladyzhensky <mlady@terpmail.umd.edu>
2025-12-03 11:46:14 -0500

    Update avoid_obstacle.py

--- avoid_obstacle.py
+++ avoid_obstacle.py
@@ -1,23 +1,26 @@
-while front_sensor > 25:
+while front_sensor > 37:
     print(front_sensor)
     update_sensors()
-    motor_on(-100, -100)
+    motor_on(-63, -100)
     time.sleep(0.05)
 motor_off()
 
+time.sleep(3)
+
 # turn left
-motors_spin(2.7, 60, -60)
-motor_off()
+motors_spin(2.6,50,-100)
 time.sleep(0.1)
 
 update_sensors()
-while right_sensor_side < 20:   # obstacle present on right side
+while right_sensor_side < 40:   # obstacle present on right side
     update_sensors()
-    motor_on(-60, 60)
+    motor_on(-68, -100)
     time.sleep(0.05)
 motor_off()
+time.sleep(0.2)
+motors_spin(1.5,-63,-100)
 
+time.sleep(.1)
 # turn right
-motors_spin(2.7, -60, 60)
-motor_off()
+motors_spin(2.4,-50,100)
 time.sleep(0.1)
